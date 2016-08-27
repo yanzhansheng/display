7ef81185ea074eeba9075c50aa3b540ee6ad2192
yanzhansheng <yzsheng_433@163.com>
2016-08-27 20:41:10 +0800

    first add content commit

---
+++ test_case.py
@@ -0,0 +1,22 @@
+import functools
+
+
+def pre_print(func):
+    @functools.wraps(func)
+    def wraps(*args,**kwargs):
+        print 'start'
+        func(*args,**kwargs)
+        print 'stop'
+    return wraps
+@pre_print
+def print_zhong(*args,**kwargs):
+    """my name is print_zhong"""
+    for i in args:
+        print i
+    for key,value in kwargs.items():
+        print key,value
+
+print_zhong(1,2,3,name='laoyan',gender='man')
+print print_zhong.__doc__
+print print_zhong.__name__
+
